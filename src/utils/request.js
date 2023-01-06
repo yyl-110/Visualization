471391a287d4dc4d1c38f4dc5005cfd29343eb3f
yyl <1040562591@qq.com>
2023-01-06 15:24:35 +0800

    feat:修改超时时间

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded
 const service = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_BASE_API : '', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 10000, // request timeout
+  timeout: 600000, // request timeout
 });
 
 // request interceptor
